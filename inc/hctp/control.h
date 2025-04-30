78ed7933e589db58e1bff3e3298bb68aee04d794
IvayloKraev <ivaylokraev@protonmail.com>
2025-04-30 17:01:40 +0300

    Combine the speed and control type

--- inc/hctp/control.h
+++ inc/hctp/control.h
@@ -1,16 +1,20 @@
 #ifndef HCTP_CONTROL_H
 #define HCTP_CONTROL_H
 
-#include "hctp/messageModel.h"
+#include "hctp/message.h"
 #include "hctp/common.h"
 #include "hctp/types.h"
 
-hctp_RESULT_t hctp_control_set_leftTurn(hctp_message_t, hctp_BUTTON_STATE);
+hctp_RESULT_t hctp_leftTurn_set(hctp_message_t, hctp_BUTTON_STATE);
 
-hctp_RESULT_t hctp_control_set_rightTurn(hctp_message_t, hctp_BUTTON_STATE);
+hctp_RESULT_t hctp_rightTurn_set(hctp_message_t, hctp_BUTTON_STATE);
 
-hctp_BUTTON_STATE hctp_control_get_leftTurn(hctp_message_t);
+hctp_RESULT_t hctp_speed_set(hctp_message_t, hctp_SPEED_t);
 
-hctp_BUTTON_STATE hctp_control_get_rightTurn(hctp_message_t);
+hctp_BUTTON_STATE hctp_leftTurn_get(hctp_message_t);
+
+hctp_BUTTON_STATE hctp_rightTurn_get(hctp_message_t);
+
+hctp_SPEED_t hctp_speed_get(hctp_message_t);
 
 #endif
--- src/control.c
+++ src/control.c
@@ -1,6 +1,6 @@
 #include "hctp/control.h"
 
-hctp_RESULT_t hctp_control_set_leftTurn(hctp_message_t message, hctp_BUTTON_STATE value) {
+hctp_RESULT_t hctp_leftTurn_set(hctp_message_t message, hctp_BUTTON_STATE value) {
     HCTP_CHECK_MESSAGE(message);
 
     HCTP_CLR_BIT(message[HCTP_CONTROL_BYTE], HCTP_LEFT_TURN_BIT);
@@ -10,7 +10,7 @@ hctp_RESULT_t hctp_control_set_leftTurn(hctp_message_t message, hctp_BUTTON_STAT
     return hctp_OK;
 }
 
-hctp_RESULT_t hctp_control_set_rightTurn(hctp_message_t message, hctp_BUTTON_STATE value) {
+hctp_RESULT_t hctp_rightTurn_set(hctp_message_t message, hctp_BUTTON_STATE value) {
     HCTP_CHECK_MESSAGE(message);
 
     HCTP_CLR_BIT(message[HCTP_CONTROL_BYTE], HCTP_LEFT_TURN_BIT);
@@ -20,10 +20,22 @@ hctp_RESULT_t hctp_control_set_rightTurn(hctp_message_t message, hctp_BUTTON_STA
     return hctp_OK;
 }
 
-hctp_BUTTON_STATE hctp_control_get_leftTurn(const hctp_message_t message) {
+hctp_RESULT_t hctp_speed_set(hctp_message_t message, hctp_SPEED_t speed) {
+    HCTP_CHECK_MESSAGE(message);
+
+    message[HCTP_SPEED_BYTE] = speed;
+
+    return hctp_OK;
+}
+
+hctp_BUTTON_STATE hctp_leftTurn_get(const hctp_message_t message) {
     return HCTP_IS_BIT_ACTIVE(message[HCTP_CONTROL_BYTE], HCTP_LEFT_TURN_BIT);
 }
 
-hctp_BUTTON_STATE hctp_control_get_rightTurn(const hctp_message_t message) {
+hctp_BUTTON_STATE hctp_rightTurn_get(const hctp_message_t message) {
     return HCTP_IS_BIT_ACTIVE(message[HCTP_CONTROL_BYTE], HCTP_RIGHT_TURN_BIT);
+}
+
+hctp_SPEED_t hctp_speed_get(hctp_message_t message){
+    return message[HCTP_SPEED_BYTE];
 }
