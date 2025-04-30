7c90633727f1c94dae4a387b764512d68d26e857
IvayloKraev <ivaylokraev@protonmail.com>
2025-04-30 17:02:19 +0300

    Remove model from the name of messageModel.h

--- inc/hctp/message.h
+++ inc/hctp/message.h
@@ -1,19 +1,19 @@
-#ifndef HCTP_MESSAGEMODEL_H
-#define HCTP_MESSAGEMODEL_H
+#ifndef HCTP_MESSAGE_H
+#define HCTP_MESSAGE_H
 
 #include "stdint.h"
 
-#define HCTP_CONTROL_BYTE 0
-#define HCTP_SPEED_BYTE 1
+#define HCTP_CONTROL_BYTE       0
+#define HCTP_SPEED_BYTE         1
 #define HCTP_MESSAGE_SIZE_BYTES 2
 
 #define HCTP_SET_BIT(byte, bit) (byte) |= (1U << (bit))
 #define HCTP_CLR_BIT(byte, bit) (byte) &= ~(1U << (bit))
 
-#define HCTP_IS_BIT_ACTIVE(value, bit) (((value) & (1 << (bit))) != 0)
+#define HCTP_IS_BIT_ACTIVE(byte, bit) (((byte) & (1 << (bit))) != 0)
 
-#define HCTP_LEFT_TURN_BIT      1
-#define HCTP_RIGHT_TURN_BIT     0
+#define HCTP_LEFT_TURN_BIT  1
+#define HCTP_RIGHT_TURN_BIT 0
 
 typedef uint8_t *hctp_message_t;
 
