2382a6ffc67c8295b695bd439372514d4a25f331
IvayloKraev <ivaylokraev@protonmail.com>
2025-05-21 23:50:24 +0300

    Add function to set the per motor state

--- src/state.c
+++ src/state.c
@@ -1,6 +1,16 @@
 #include "hcst/state.h"
 
-static hcst_result_t hcst_motor_set(hcst_message_t message, hsct_motor_bit_t motor, hcst_motor_state_handler_t state) {
+static hcst_result_t hcst_motor_state_set(hcst_motor_state_handler_t state, hcst_state_t powered, hcst_state_t direction, hcst_speed_t speed) {
+    hcst_MOTOR_STATE_CHECK(state);
+
+    state->power = powered;
+    state->direction = direction;
+    state->speed = speed;
+
+    return hcst_OK;
+}
+
+static hcst_result_t hcst_state_set(hcst_message_t message, hsct_motor_bit_t motor, hcst_motor_state_handler_t state) {
     hcst_MESSAGE_CHECK(message);
     if (motor >= hcst_NUM_OF_MOTORS) return hcst_INVALID;
 
