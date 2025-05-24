6c992d72c7304b54ddd947e4060719514c995ef7
IvayloKraev <ivaylokraev@protonmail.com>
2025-05-24 21:16:37 +0300

    Inline the validation

--- src/state.c
+++ src/state.c
@@ -1,9 +1,9 @@
 #include "hcst/state.h"
 
 static hcst_error_t hcst_message_set(hcst_message_t message, hsct_motor_enum motor, hcst_motorState_t *state) {
-    hcst_MESSAGE_CHECK(message);
-    hcst_MOTOR_STATE_CHECK(state);
-    if (motor > hcst_NUM_OF_MOTORS) return hcst_INVALID;
+    if (message == NULL) return hcst_error_nullPtr;
+    if (motor < 0 || motor >= hcst_NUM_OF_MOTORS) return hcst_error_invalidMotor;
+    if (state == NULL) return hcst_error_nullPtr;
 
     hcst_BIT_CLEAR(message[hcst_MOTOR_POWERED_BYTE], motor);
     hcst_BIT_SET(message[hcst_MOTOR_POWERED_BYTE], state->power, motor);
@@ -14,17 +14,18 @@ static hcst_error_t hcst_message_set(hcst_message_t message, hsct_motor_enum mot
     hcst_BYTE_CLEAR(message[hcst_SPEED_BYTE + motor]);
     hcst_BYTE_SET(message[hcst_SPEED_BYTE + motor], state->speed);
 
-    return hcst_OK;
+    return hcst_error_none;
 }
 
 static hcst_error_t hcst_message_get(hcst_message_t message, hsct_motor_enum motor, hcst_motorState_t *state) {
-    hcst_message_set(message, 6, state);
-    hcst_MESSAGE_CHECK(message);
-    hcst_MOTOR_STATE_CHECK(state);
+    if (message == NULL) return hcst_error_nullPtr;
+    if (motor < 0 || motor > hcst_NUM_OF_MOTORS) return hcst_error_invalidMotor;
+    if (state == NULL) return hcst_error_nullPtr;
+
 
     state->power = hcst_BIT_IS_ACTIVE(message[hcst_MOTOR_POWERED_BYTE], motor);
     state->direction = hcst_BIT_IS_ACTIVE(message[hcst_MOTOR_DIRECTION_BYTE], motor);
     state->speed = message[hcst_SPEED_BYTE + motor];
 
-    return hcst_OK;
+    return hcst_error_none;
 }
