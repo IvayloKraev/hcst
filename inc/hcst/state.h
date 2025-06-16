12baf477f8b53b62ca519ac6b2c451e1871f0f14
IvayloKraev <ivaylokraev@protonmail.com>
2025-06-16 18:17:55 +0300

    Remove the init and deinit for motor state

--- inc/hcst/state.h
+++ inc/hcst/state.h
@@ -17,32 +17,6 @@ typedef struct {
     hcst_speed_t speed;
 } hcst_motorState_t;
 
-/*! \brief Allocates the memory for a motor state
- *
- *  \param motorState Pointer to the motorState
- *
- *  \return The error code - 0 if successful, 1 and above for error \ref hcst_error_t
- */
-inline hcst_error_t hcst_motorState_init(hcst_motorState_t **motorState) {
-    if (*motorState != NULL) return hcst_error_unreleasedMemory;
-    *motorState = (hcst_motorState_t*) malloc(sizeof(hcst_motorState_t));
-    if (!*motorState) return hcst_error_outOfMemory;
-    return hcst_error_none;
-}
-
-/*! \brief Removes the allocated the memory for a motor state
- *
- *  \param motorState Pointer to the motorState
- *
- *  \return The error code - 0 if successful, 1 and above for error \ref hcst_error_t
- */
-inline hcst_error_t hcst_motorState_deinit(hcst_message_t **motorState) {
-    if (motorState == NULL) return hcst_error_nullPtr;
-    free(*motorState);
-    *motorState = NULL;
-    return hcst_error_none;
-}
-
 /*! \brief Writes the motor state in the message
  *
  *  \param message The message to write the motor state
